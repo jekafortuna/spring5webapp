046872ca25e7c029264ce3300d36f7f75977ce46
Yevhenii Fortuna <evgeniy.fortuna@gmail.com>
2018-10-16 14:41:50 +0300

    adding JPA directory

--- src/main/java/ua/kpi/spring5webapp/model/Book.java
+++ src/main/java/ua/kpi/spring5webapp/model/Book.java
@@ -91,12 +91,12 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", publisher='" + publisher + '\'' +
-                ", authors=" + authors +
+        return "Book {" +
+                " id = " + id +
+                ", title = '" + title + '\'' +
+                ", isbn = '" + isbn + '\'' +
+                ", publisher = '" + publisher + '\'' +
+                ", authors = " + authors +
                 '}';
     }
 }
